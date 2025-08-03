2f74d98a579bffd6bb83b72dc43c7e6c8b15abf4
{Tran Tuan Anh} <{trantuananh070405@gmail.com}>
2025-08-03 14:04:28 +0700

    cập nhật .sql

--- database/schema.sql
+++ database/schema.sql
@@ -79,3 +79,4 @@ create table group_members (
     foreign key (user_id) references users(user_id) on delete cascade
 );
 
+INSERT INTO `users` VALUES (1,'danny07','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','danny@gmail.com','2025-08-01 16:09:30'),(2,'danny04','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','danny04@gmail.com','2025-08-02 23:25:05'),(3,'test','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','test@gmail.com','2025-08-02 23:31:57'),(4,'test2','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','test2@gmail.com','2025-08-02 23:36:03'),(5,'test3','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','test3@gmail.com','2025-08-02 23:37:29'),(6,'test4','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','test4@gmail.com','2025-08-02 23:37:55'),(7,'test6','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','test6@gmail.com','2025-08-02 23:40:26');
